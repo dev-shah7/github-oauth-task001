917f97eb7d99c8d79b912205d3d39f28878b7867
dev-shah7 <s.tech4747@gmail.com>
2025-02-08 05:10:27 +0500

    add ag grid

--- task001-FE/src/app/app.component.ts
+++ task001-FE/src/app/app.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Import AG Grid Component
+import { ColDef, ModuleRegistry } from 'ag-grid-community'; // Import ModuleRegistry
+import { ClientSideRowModelModule } from 'ag-grid-community'; // Import required module
+
+// Register AG Grid modules
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, AgGridAngular],
+
   template: `
-    <h2>Task 001</h2>
+    <!-- The AG Grid component -->
+    <ag-grid-angular
+      [rowData]="rowData"
+      [columnDefs]="colDefs"
+      rowModelType="clientSide"
+      class="ag-theme-alpine"
+      style="width: 600px; height: 400px;"
+    ></ag-grid-angular>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'Task 001';
+
+  rowData = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
 }
