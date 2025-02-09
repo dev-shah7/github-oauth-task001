bc5ca491f3306add3266a4da16bfbb20f7feb78f
dev-shah7 <s.tech4747@gmail.com>
2025-02-09 18:33:06 +0500

    fix filters

--- task001-FE/src/app/components/organizations/organizations.component.ts
+++ task001-FE/src/app/components/organizations/organizations.component.ts
@@ -91,11 +91,15 @@ export class OrganizationsComponent implements OnInit {
   ];
 
   searchText: string = '';
-  defaultColDef = {
+  defaultColDef: ColDef = {
     sortable: true,
     filter: true,
     floatingFilter: true,
     resizable: true,
+    filterParams: {
+      buttons: ['reset', 'apply'],
+      closeOnApply: true,
+    },
   };
 
   private searchSubject = new Subject<string>();
@@ -172,12 +176,9 @@ export class OrganizationsComponent implements OnInit {
       next: (data) => {
         this.rowData = data;
         if (data.length > 0) {
-          this.columnDefs = Object.keys(data[0]).map((key) => ({
-            field: key,
-            headerName: this.formatHeaderName(key),
-            sortable: true,
-            filter: true,
-          }));
+          this.columnDefs = Object.keys(data[0]).map((key) =>
+            this.getColumnDef(key, data[0][key])
+          );
         }
         this.loading = false;
       },
@@ -250,20 +251,13 @@ export class OrganizationsComponent implements OnInit {
         next: (data) => {
           this.rowData = data;
           if (data.length > 0) {
-            this.columnDefs = Object.keys(data[0]).map((key) => ({
-              field: key,
-              headerName: this.formatHeaderName(key),
-              sortable: true,
-              filter: true,
-              floatingFilter: true,
-              resizable: true,
-            }));
+            this.columnDefs = Object.entries(data[0]).map(([key, value]) =>
+              this.getColumnDef(key, value)
+            );
           }
           if (this.searchText) {
             setTimeout(() => {
-              this.agGrid.api.setFilterModel({
-                quickFilter: { filter: this.searchText },
-              });
+              this.agGrid.api.setGridOption('quickFilterText', this.searchText);
             });
           }
           this.loading = false;
@@ -297,11 +291,100 @@ export class OrganizationsComponent implements OnInit {
 
   onSearchChange(value: string) {
     this.searchText = value;
-    this.searchSubject.next(value);
+    if (this.agGrid?.api) {
+      this.agGrid.api.setGridOption('quickFilterText', value);
+    }
   }
 
   clearSearch() {
     this.searchText = '';
     this.searchSubject.next('');
+  }
+
+  private getColumnDef(key: string, value: any): ColDef {
+    const baseConfig = {
+      field: key,
+      headerName: this.formatHeaderName(key),
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+      resizable: true,
+    };
+
+    if (value === null || value === undefined) {
+      return { ...baseConfig, filter: 'agTextColumnFilter' };
+    }
+
+    switch (typeof value) {
+      case 'number':
+        return {
+          ...baseConfig,
+          filter: 'agNumberColumnFilter',
+          filterParams: {
+            buttons: ['apply', 'reset'],
+            closeOnApply: true,
+            allowedCharPattern: '\\d\\-\\.',
+          },
+        };
+      case 'boolean':
+        return {
+          ...baseConfig,
+          filter: 'agSetColumnFilter',
+          filterParams: {
+            values: ['true', 'false'],
+          },
+        };
+      case 'object':
+        if (value instanceof Date) {
+          return {
+            ...baseConfig,
+            filter: 'agDateColumnFilter',
+            filterParams: {
+              buttons: ['apply', 'reset'],
+              closeOnApply: true,
+              comparator: (
+                filterLocalDateAtMidnight: Date,
+                cellValue: string
+              ) => {
+                const dateAsString = cellValue;
+                const dateParts = dateAsString.split('-');
+                const cellDate = new Date(
+                  Number(dateParts[0]),
+                  Number(dateParts[1]) - 1,
+                  Number(dateParts[2])
+                );
+                if (
+                  filterLocalDateAtMidnight.getTime() === cellDate.getTime()
+                ) {
+                  return 0;
+                }
+                if (cellDate < filterLocalDateAtMidnight) {
+                  return -1;
+                }
+                if (cellDate > filterLocalDateAtMidnight) {
+                  return 1;
+                }
+                return 0;
+              },
+            },
+          };
+        }
+        return {
+          ...baseConfig,
+          filter: 'agTextColumnFilter',
+          valueGetter: (params) => {
+            return JSON.stringify(params.data[key]);
+          },
+        };
+      default:
+        return {
+          ...baseConfig,
+          filter: 'agTextColumnFilter',
+          filterParams: {
+            buttons: ['apply', 'reset'],
+            closeOnApply: true,
+          },
+        };
+    }
   }
 }
