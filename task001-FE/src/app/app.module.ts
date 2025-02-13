515110835eb3b7ac0829986b6364ab2de029fa56
dev-shah7 <s.tech4747@gmail.com>
2025-02-13 23:27:34 +0500

    fix issues

--- task001-FE/src/app/app.module.ts
+++ task001-FE/src/app/app.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  // ... other imports ...
-  imports: [
-    // ... other modules ...
-    MatSnackBarModule,
-  ],
-  // ... rest of the module definition
+  imports: [MatSnackBarModule],
 })
 export class AppModule {}
